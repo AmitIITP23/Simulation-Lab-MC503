8ac5b4dd98871034d081cc2848a0803eb89c0435
Amit Kumar Singh <uohamit@gmail.com>
2021-11-22 00:06:20 +0530

    Add files via upload

---
+++ Assignment 12/Ques1.R
@@ -0,0 +1,53 @@
+library(nleqslv)
+
+c = 1
+k = 1
+n1 = 50
+
+sample_gen <- function(n,c,k){
+  
+  u_vect = runif(n)
+  x <- numeric(0)
+  i = 1
+  for(u in u_vect){
+    x[i] <- (((1-u)^(-1/k))-1)^(1/c)
+    i = i + 1
+  }
+  return(x)
+}
+
+mle_list <- matrix(0,nrow = 100, ncol = 2)
+
+for (i in 1:100) {
+  
+  vec1 = sample_gen(n1,c,k)
+  
+  est1 <- function(para){
+    c <- para[1]
+    k <- para[2]
+    y <- numeric(0)
+    
+    y[1] = n1/c + sum(log(vec1)) - (k+1)*sum((vec1^c)*log(vec1)/(1+vec1^c))
+    y[2] = n1/k - sum(log(1+vec1^c))
+    return(y)
+  }
+  
+  xstr <- c(c,k)
+  mle1 <- nleqslv(xstr, est1,method='Newton',jacobian=TRUE)
+  mle_list[i,] <- mle1$x
+}
+
+mle_c = mean(mle_list[,1])
+mle_k = mean(mle_list[,2])
+
+var_mat <- solve(-mle1$jac)
+
+z = 1.96
+ci_c_l = mle_c - z*(var_mat[1,1])^(1/2)
+ci_c_u = mle_c + z*(var_mat[1,1])^(1/2)
+
+ci_k_l = mle_k - z*(var_mat[2,2])^(1/2)
+ci_k_u = mle_k + z*(var_mat[2,2])^(1/2)
+
+cat("\nMLE of c and k: ",mle_c,mle_k,
+    "\nCI of c & k:\n",ci_c_l,ci_c_u,"\t",ci_k_l,ci_k_u)
